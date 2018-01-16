e5213fd2348262086a5fdc1fd4f01101992ffbc1
Csaba Fehér <nexusegyesulet@gmail.com>
2018-01-16 13:56:10 +0100

    v0.0

---
+++ IPCount/IpControll.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IPCount
+{
+    class IpControll
+    {
+        byte ip;
+        public byte Ip { get => ip; set => ip = value; }
+        string binIPAddr;
+        public string BinIPAddr { get => binIPAddr; set => binIPAddr = value; }
+        Label lbl;
+        public Label Lbl { get => lbl; set => lbl = value; }
+        public string IpOctet {
+            get
+            {
+                return IpOctet;
+            }
+            set
+            {
+                bool ok = true;
+
+                if (value.Length != 0)
+                {
+                    lbl = new Label();
+                    ipOctet = value;
+                    ok = byte.TryParse(ipOctet, out byte ip);
+                    if (!ok) MessageBox.Show("Rossz IP cím"); 
+                    Ip = ip;
+                    binIPAddr = DecToBin(ip);
+                    lbl.Content = binIPAddr;
+                    
+                }
+            }
+        }
+
+        
+
+        string ipOctet;
+
+        
+        //public IpControll()
+        //{ }
+        
+
+        //public IpControll(string ipOctet)
+        //{
+            
+        //    bool ok = true;
+
+        //    if (ipOctet.Length != 0)
+        //    {
+        //        ok = byte.TryParse(ipOctet, out byte ip);
+        //        if (!ok) throw new Exception("Rossz ip cím");
+        //        Ip = ip;
+        //        binIPAddr = DecToBin(ip);
+        //        lbl.Content = binIPAddr;
+        //    }
+        //}
+
+        protected string DecToBin(byte i)
+        {
+            string result = "";
+            while (i>0)
+            {
+                int remainder = i % 2;
+                result = remainder.ToString() + result;
+                i /= 2;
+            }
+            int l = result.Length;
+            for (int z = 0; z < 8-l; z++)
+            {
+                result = "0" + result;
+            }
+            return result;
+        }
+
+
+
+        
+    }
+}
---
+++ IPCount/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IPCount
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        TextBox iptb;
+        TextBox mtb;
+        Label point;
+        Label binIP;
+        TextBox cIDR;
+        IpControll ik;
+        List<TextBox> IpOctets = new List<TextBox>();
+        List<Label> BinIPs = new List<Label>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ik = new IpControll();
+            for (int i = 0; i < 4; i++)
+            {
+                iptb = new TextBox
+                {
+                    
+                    
+                    Width = 30,
+                    Height = 20
+                };
+                point = new Label
+                {
+                    Content = "."
+                };
+                binIP = new Label
+                {
+                    Content = "00000000",
+                    Height = 30,
+                    Width = 70,
+                    Background = new SolidColorBrush(Colors.LightGray)
+                };
+
+                BinIPs.Add(ik.Lbl);
+                IpOctets.Add(iptb);
+                iptb.TextChanged += Iptb_TextChanged;
+                SpIPInput.Children.Add(iptb);
+                if (i == 3) point.Content = "/";
+                SpIPInput.Children.Add(point);
+                LblSpBinIp.Children.Add(binIP);
+                
+            }
+            cIDR = new TextBox()
+            {
+                Width = 30,
+                Height = 20
+            };
+            SpIPInput.Children.Add(cIDR);
+
+            for (int i = 0; i < 4; i++)
+            {
+              
+                mtb = new TextBox
+                {
+                    Width = 30,
+                    Height = 20
+                };
+                point = new Label
+                {
+                    Content = "."
+                };
+                SpMaskInput.Children.Add(mtb);
+                if (i!=3)
+                {
+                    SpMaskInput.Children.Add(point);
+                }
+                
+            }
+
+
+
+        }
+
+        private void Iptb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+
+                binIP = new Label
+                {
+                    Background = new SolidColorBrush(Colors.Coral)
+                };
+                
+                string s = IpOctets[i].Text;
+                
+                if (s!="")
+                {
+                    ik.IpOctet = s;
+                    
+                    binIP.Content = BinIPs[i].Content;
+                }
+                
+            }
+        }
+    }
+}
